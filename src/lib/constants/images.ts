9b9842ba1f1c27d55b97aff5b5eafb27f463b734
henrychris <64085210+henrychris@users.noreply.github.com>
2025-10-10 00:01:59 +0100

    Update images.ts

--- src/lib/constants/images.ts
+++ src/lib/constants/images.ts
@@ -1,3 +1,3 @@
 export const IMAGES = {
-	ME: 'https://res.cloudinary.com/dit0zbles/image/upload/v1760049170/me-github_dampvy.jpg'
+	ME: 'https://res.cloudinary.com/dit0zbles/image/upload/v1760050910/WhatsApp_Image_2025-10-10_at_00.01.02_ymkk59.jpg'
 } as const;
