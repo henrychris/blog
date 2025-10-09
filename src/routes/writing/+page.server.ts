06be9c9de27178a40895cbf3b2cc1f80dca25116
henrychris <64085210+henrychris@users.noreply.github.com>
2025-10-09 23:20:47 +0100

    Update +page.server.ts

--- src/routes/writing/+page.server.ts
+++ src/routes/writing/+page.server.ts
@@ -1,5 +1,5 @@
 import type { Post } from '$lib/types';
-import type { EntryGenerator } from '../posts/[slug]/$types.js';
+import type { EntryGenerator } from './[slug]/$types';
 
 async function loadPosts() {
 	let posts: Post[] = [];
