f3ecc3001c4ca45396e419b53118ddb994d14da3
henrychris <64085210+henrychris@users.noreply.github.com>
2025-10-10 12:02:36 +0100

    add author to quotes

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	const quotes = [
-		"Despite everything, it's still you.",
-		"I kind of feel like I'm on drugs when I'm with you. Not that I do drugs. Unless you do drugs. Then I do drugs all the time, every drug.",
-		"Everyone of us has been special since the day we were born - we're free.",
-		"Sometimes, life's a bitch and then you keep living.",
-		"No risk, full push."
+	type Quote = {
+		text: string;
+		author?: string;
+	};
+
+	const quotes: Quote[] = [
+		{
+			text: "Despite everything, it's still you.",
+			author: 'Undertale'
+		},
+		{
+			text: "I kind of feel like I'm on drugs when I'm with you. Not that I do drugs. Unless you do drugs. Then I do drugs all the time, every drug.",
+			author: 'Scott Pilgrim vs The World'
+		},
+		{
+			text: "Everyone of us has been special since the day we were born - we're free.",
+			author: 'Attack On Titan'
+		},
+		{
+			text: "Sometimes, life's a bitch and then you keep living.",
+			author: 'Bojack Horseman'
+		},
+		{
+			text: 'No risk, full push.',
+			author: 'Max Verstappen'
+		}
 	];
 	const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
@@ -33,7 +53,10 @@
 <footer class="border-t">
 	<div class="mt-2 flex flex-col items-start justify-between gap-2 sm:flex-row sm:items-center">
 		<div class="w-full max-w-md">
-			<p class="text-gray-600 italic">"{randomQuote}"</p>
+			<p class="text-gray-600 italic">"{randomQuote.text}"</p>
+			{#if randomQuote.author}
+				<p class="text-xs text-gray-500">— {randomQuote.author}</p>
+			{/if}
 		</div>
 
 		<div
