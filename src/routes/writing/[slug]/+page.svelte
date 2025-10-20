fd01004b4ad97c0c2d234239d87b3ab38b11ba07
henrychris <64085210+henrychris@users.noreply.github.com>
2025-10-20 23:47:04 +0100

    undo style changes

--- src/routes/writing/[slug]/+page.svelte
+++ src/routes/writing/[slug]/+page.svelte
@@ -12,30 +12,28 @@
 	{/each}
 </svelte:head>
 
-<div class="flex items-center justify-center">
-	<div class="flex max-w-7xl gap-8">
-		<article class="flex max-w-3xl flex-col gap-8">
-			<div class="flex flex-col gap-2">
-				<h1 class="text-2xl font-bold">{data.meta.title}</h1>
-				<p class="text-sm text-gray-500">{formatDate(data.meta.date)}</p>
-				{#if data.meta.categories.length > 0}
-					<div class="flex gap-2">
-						{#each data.meta.categories as category (category)}
-							<span class="text-xs text-gray-600">#{category}</span>
-						{/each}
-					</div>
-				{/if}
-			</div>
+<div class="flex max-w-7xl gap-8">
+	<article class="mx-auto max-w-prose">
+		<div class="mb-8 flex flex-col gap-1">
+			<h1 class="text-2xl font-bold">{data.meta.title}</h1>
+			<p class="text-sm text-gray-500">{formatDate(data.meta.date)}</p>
+			{#if data.meta.categories.length > 0}
+				<div class="mt-2 flex gap-2">
+					{#each data.meta.categories as category (category)}
+						<span class="text-xs text-gray-600">#{category}</span>
+					{/each}
+				</div>
+			{/if}
+		</div>
 
-			<div class="article-content">
-				<data.content />
-			</div>
-		</article>
+		<div class="article-content">
+			<data.content />
+		</div>
+	</article>
 
-		<aside class="hidden w-64 lg:block">
-			<TableOfContents />
-		</aside>
-	</div>
+	<aside class="hidden w-64 lg:block">
+		<TableOfContents />
+	</aside>
 </div>
 
 <style>
